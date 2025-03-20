a26d5d428377c6eaaff4c71111dd6e4a50f2c4a2
micaelaperillo <micaelaperillo26@gmail.com>
2025-03-19 23:25:10 -0300

    remove unused code and delete old files

--- main.py
+++ main.py
@@ -9,19 +9,6 @@ from src.utils import average_probability_of_capture, pokeball_effectiveness, \
     analyze_status_and_capture_with_capture_rate
 
 if __name__ == "__main__":
-    # factory = PokemonFactory("pokemon.json")
-    # with open(f"{sys.argv[1]}", "r") as f:
-    #     config = json.load(f)
-    #     ball = config["pokeball"]
-    #     pokemon = factory.create(config["pokemon"], 100, StatusEffect.NONE, 1)
-
-    #     # for i in range(100, 1, -1):
-    #     #     pokemon = factory.create(config["pokemon"], 100, StatusEffect.NONE, i / 100)
-    #     #     print(pokemon.current_hp)
-
-    #     print("No noise: ", attempt_catch(pokemon, ball))
-    #     for _ in range(10):
-    #         print("Noisy: ", attempt_catch(pokemon, ball, 0.15))
 
     # Ex: 1a
     average_probability_of_capture()
